3c58d0d420db998e0c5a1d365c1a2992247606ec
PrzemekPytlak <przemyslaw.pytlak@gmail.com>
2021-04-18 23:11:23 +0200

    fix after org solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,8 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveView}></div>
+
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -86,7 +88,8 @@ function App() {
               skinColor={skinColor}
               clothesColor={clothesColor}
             />
-          </div>
+         </div>
+
       </MaxWidthWrapper>
     </main>
   );
